ff198713497ccfb080e2e3612048950615c186cb
Ke2ke2 <dieketsengmafatle98@gmail.com>
2023-01-09 10:22:34 -0800

    lists.h

--- lists.h
+++ lists.h
@@ -2,35 +2,22 @@
 
 #define LISTS_H
 
-
-
 #include <stdlib.h>
 
 
 
 /**
- *
- *  * struct listint_s - singly linked list
- *
- *   * @n: integer
- *
- *    * @next: points to the next node
- *
- *     *
- *
- *      * Description: singly linked list node structure
- *
- *       * for Holberton project
- *
- *        */
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ */
 
 typedef struct listint_s
 
 {
-
-		int n;
-
-			struct listint_s *next;
+	int n;
+	struct listint_s *next;
 
 } listint_t;
 
@@ -46,4 +33,4 @@ int check_cycle(listint_t *list);
 
 
 
-#endif /* LISTS_H */
+#endif
